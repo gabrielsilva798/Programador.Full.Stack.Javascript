1e1b904591267f46c9877a19b69d2d6d7b5ccd2c
Gabriel_Silva <gabrielsilva7988@gmail.com>
2024-03-10 20:34:25 -0300

    Aula 73 - Recursos Nativos do Browser

---
+++ modulo6-js_script/operacoes.js
@@ -0,0 +1,15 @@
+//                EXTRAINDO ELEMENTO DE UMA STRING
+
+const planeta = "Jupiter";
+
+/*posso extraí igual nos Arrays, colocando entre conchetes[]
+ *começa a contar do ZERO */
+console.log(planeta[2]);
+
+console.log(planeta.length)
+
+//vai sair planetaR
+console.log(planeta+'terra'[3])
+
+//aqui, obviamente, vai sair 'I', porque pega do geral entre aspas()
+console.log((planeta+'terra')[3])
---
+++ modulo6-js_script/recursos_nativos.js
@@ -0,0 +1,18 @@
+//                         alguns nõa vão aparecer aqui no VS CODE, mas aparecem no navegaador
+//não vi diferenca do .LOG
+console.info('planeta')
+
+//CONSOLE.WARN usado para exibir mensagens de aviso.
+console.warn("CONSOLE.WARN: usado para exibir mensagens de aviso.")
+
+//CONSOLE.WARN, exibi uma mensagem em VERMELHO indicando erro.
+console.error("CONSOLE.WARN: exibi uma mensagem em VERMELHO indicando erro.");
+
+//ALERT: exibi um alerta na tela do usuário.
+//alert("ALERT: exibi um alerta na tela do usuário.");
+
+//CONFIRM: aprece uma caixa para confirmar algo, com as opções CANCEL e OK.
+//confirm("CONFIRM: aprece uma caixa para confirmar algo")
+
+//PROMPT: igual o confirm, mas aparece tbm um espaço para inserir dados de texto e onfirmar
+prompt("PROMPT: igual o confirm, mas aparece tbm um espaço para inserir dados de text tbm.")
