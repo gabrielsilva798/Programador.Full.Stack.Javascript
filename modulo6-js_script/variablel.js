431786cabc319b4ad38d202ab83d4806bc19d19e
Gabriel_Silva <gabrielsilva7988@gmail.com>
2024-03-12 20:18:03 -0300

    variáveis

---
+++ modulo6-js_script/variablel.js
@@ -0,0 +1,19 @@
+//CONST cria constante, não podem ser alterada
+const constante = 10;
+console.log(constante);
+
+//LET cria variáveis MULTÁVEIS.
+let variavel_let = 10;
+console.log(constante);
+variavel_let = 39;
+console.log(variavel_let)
+
+//VAR igual o LET, cria variáveis MULTÁVEIS
+var variavel_var = 10;
+console.log(constante);
+variavel_var = 39;
+console.log(variavel_let)
+
+//VAR e LET podem ser inicializada sem valor, mas CONST não pode.
+var noValue;
+let semValor;
