ad9e9e945a1d5cfa5663d2c5a6cd80e059fb232e
Gabriel_Silva <gabrielsilva7988@gmail.com>
2024-03-14 23:09:33 -0300

    priemiro desafio condicional

---
+++ modulo7-condicional/2-firstChalenge/condicional.js
@@ -0,0 +1,26 @@
+let nome = prompt("Qual seu nome? ");
+
+let velocidade = 0;
+
+let velocidadeInicial = prompt("Qual velocidade gostaria de acelerar a nave?");
+
+let confirmacao = confirm("Velocidade da nave indo para " + velocidadeInicial + "km/s?");
+
+while (confirmacao == false) {
+    velocidadeInicial = prompt("Qual velocidade gostaria de acelerar a nave?");
+    confirmacao = confirm("Velocidade da nave indo para " + velocidadeInicial + "km/s?");
+}
+
+velocidade = velocidadeInicial;
+
+if(velocidade <= 0 || velocidade == null) {
+    alert("Nave está parada. Considere partir e aumente a velocidade")
+} else if (velocidade < 40) {
+    alert("Você está devagar, podemos aumentar mais")
+} else if (velocidade >= 40 && velocidade < 80) {
+    alert("Parece uma boa velocidade para manter")  
+} else if (velocidade >=80 && velocidade < 100) {
+    alert("Velocidade alta, considere diminuir")
+} else {
+    alert("Velocidade perigosa. Controle automâtico forçado")
+}
