40337046b402161e362d206a9267bd11c94cdf8f
Gabriel_Silva <gabrielsilva7988@gmail.com>
2024-03-30 20:17:09 -0300

    duas primeiras aulas do môdulo 9- FUNCTIONS

---
+++ modulo9- function/function.js
@@ -0,0 +1,12 @@
+let resultado = 0;
+    
+    function contador(valor){
+        resultado +=valor;
+    
+        return resultado;
+    }
+
+    console.log(contador(5));
+    console.log(contador(15));
+    console.log(contador(30));
+    console.log(contador());
